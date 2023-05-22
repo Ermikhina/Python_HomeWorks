43f41019ff04736a495312a079858f08ea56d60d
Natal <nat_n_e@mail.ru>
2023-05-22 12:04:08 +0300

    Modifided Task02

--- Task02.py
+++ Task02.py
@@ -14,6 +14,9 @@
 # Задача 2: Найдите сумму цифр трехзначного числа. 
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+# number = int(input("Введите 3-хзначное число: "))
+# sumdig = number//100 + number//10%10+number%10
+# print(f"Сумма цифр числа {number} = {sumdig}")
 number = int(input("Введите 3-хзначное число: "))
-sumdig = number//100 + number//10%10+number%10
-print(f"Сумма цифр числа {number} = {sumdig}")
+sumdig = number//100 + number//10%10 + number%10
+print(f"Сумма цифр числа {number} = {number//100} + {number//10%10} + {number%10} = {sumdig}")
